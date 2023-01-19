791e9f2a5894322944c0db9ce3306cdae28a5029
Hit9it <hit9it@gmail.com>
2023-01-19 16:49:25 +0200

    lesson_1

--- src/App.js
+++ src/App.js
@@ -1,14 +1,23 @@
-import {Simpson} from "./Simpsons/Simpson";
+import {Simpson} from "./Animasion/Simpsons/Simpson";
+import Rick_and_Morty, {Characters} from "./Animasion/Rick_and_Morty/Characters";
+import Character from "./Animasion/Rick_and_Morty/Character";
 
 const App = () => {
     return (
         <div>
-            <Simpson name = {'Homer'} surname = {'Simpson'} image = {'https://upload.wikimedia.org/wikipedia/en/0/02/Homer_Simpson_2006.png'}/>
-            <Simpson name = {'Marge'} surname = {'Simpson'} image = {'https://upload.wikimedia.org/wikipedia/ru/0/0b/Marge_Simpson.png'}/>
-            <Simpson name = {'Bart'} surname = {'Simpson'} image = {'https://upload.wikimedia.org/wikipedia/en/a/aa/Bart_Simpson_200px.png'}/>
-            <Simpson name = {'Lisa'} surname = {'Simpson'} image = {'https://upload.wikimedia.org/wikipedia/en/e/ec/Lisa_Simpson.png'}/>
-            <Simpson name = {'Meggie'} surname = {'Simpson'} image = {'https://upload.wikimedia.org/wikipedia/ru/9/9d/Maggie_Simpson.png'}/>
+            <div>
+                <Simpson name = {'Homer'} surname = {'Simpson'} image = {'https://upload.wikimedia.org/wikipedia/en/0/02/Homer_Simpson_2006.png'}/>
+                <Simpson name = {'Marge'} surname = {'Simpson'} image = {'https://upload.wikimedia.org/wikipedia/ru/0/0b/Marge_Simpson.png'}/>
+                <Simpson name = {'Bart'} surname = {'Simpson'} image = {'https://upload.wikimedia.org/wikipedia/en/a/aa/Bart_Simpson_200px.png'}/>
+                <Simpson name = {'Lisa'} surname = {'Simpson'} image = {'https://upload.wikimedia.org/wikipedia/en/e/ec/Lisa_Simpson.png'}/>
+                <Simpson name = {'Meggie'} surname = {'Simpson'} image = {'https://upload.wikimedia.org/wikipedia/ru/9/9d/Maggie_Simpson.png'}/>
+            </div>
+
+            <div>
+                <Characters/>
+            </div>
         </div>
+
     );
 };
 export {App};
